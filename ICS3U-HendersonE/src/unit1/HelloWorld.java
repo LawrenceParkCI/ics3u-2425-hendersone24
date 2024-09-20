384e642fd4cb3c8e0abd1163c468f5f883e83e58
350258218 <350258218@C-MXL2393F1V.tdsb.on.ca>
2024-09-20 11:11:07 -0400

    Adding ICS3U project with HelloWorld class

---
+++ ICS3U-HendersonE/src/unit1/HelloWorld.java
@@ -0,0 +1,22 @@
+package unit1;
+/**
+ * Description: This program prints Hello World, and
+ * a second statement <p>
+ * Date Sept 19, 2024
+ * @author Ethan Henderson 
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//This prints hello to the system console
+		System.out.print("Hello World!!");
+		System.out.println("My Name Is Ethan");
+		
+		/*
+		 * This is a block comment
+		 * You can add notes on multiple lines.
+		 */
+
+	}
+
+}
