5e8bce3de9d43db2ecc53da48aa587fb2e4f6914
350258218 <350258218@C-MXL2393F1V.tdsb.on.ca>
2024-09-24 10:26:31 -0400

    Worked On

---
+++ ICS3U-HendersonE/src/unit1/AboutMe.java
@@ -0,0 +1,27 @@
+package unit1;
+/**
+ * Description: This program prints basic information about myself
+ * a second statement. <br>
+ * Date: September 20th, 2024
+ * @author Ethan Henderson
+ */
+
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Name: Ethan Henderson"); 
+		System.out.println("Grade: 11");
+		System.out.println("Teacher: Ms. Kemp");
+		System.out.println("School: LPCI in Toronto");
+		System.out.println("!GO LPCI PANTHERS!");
+		System.out.println("____________________________");
+		;
+		
+		//Schedule - Part 2
+		System.out.println("My School Schedule");
+		System.out.format("Monday: %-17sTime: %10s", "Tech - Period 1|", "9:00am - 10:15am");
+		
+	}
+
+}
