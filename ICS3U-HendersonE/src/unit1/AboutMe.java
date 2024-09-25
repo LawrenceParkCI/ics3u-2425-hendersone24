569fb96893e351abfc88cf3215e83315892162be
350258218 <350258218@C-MXL2393F1V.tdsb.on.ca>
2024-09-25 10:54:09 -0400

    Assignment 1 - Mostly Completed

--- ICS3U-HendersonE/src/unit1/AboutMe.java
+++ ICS3U-HendersonE/src/unit1/AboutMe.java
@@ -20,37 +20,37 @@ public class AboutMe {
 		
 		//Schedule - Part 2
 		System.out.println("My School Schedule | Sept 23-27, 2024 \n");
-		System.out.format("Monday: %-10s Tech %7s", "Period #1:", "| Time: 9:00 - 10:15 \n");
-		System.out.format("%18s Math %7s", "Period #2:", "| Time: 10:15 - 11:40 \n");
+		System.out.format("Monday: %-10s Tech    %7s", "Period #1:", "| Time: 9:00 - 10:15 \n");
+		System.out.format("%18s Math    %7s", "Period #2:", "| Time: 10:15 - 11:40 \n");
 		System.out.format("%18s English %7s", "Period #3:", "| Time: 12:40 - 1:55 \n");
 		System.out.format("%18s Biology %7s", "Period #4:", "| Time: 1:50 - 3:15 \n");
 		
 		
 		System.out.println("____________________________________________________");
 		System.out.println("");
-		System.out.format("Tuesday: %-10s Math %7s", "Period #1:", "| Time: 9:00 - 10:15 \n");
-		System.out.format("%19s Tech %-7s", "Period #2:", "| Time: 10:15 - 11:40 \n");
+		System.out.format("Tuesday: %-10s Math    %7s", "Period #1:", "| Time: 9:00 - 10:15 \n");
+		System.out.format("%19s Tech    %-7s", "Period #2:", "| Time: 10:15 - 11:40 \n");
 		System.out.format("%19s Biology %-7s", "Period #3:", "| Time: 12:40 - 1:55 \n");
 		System.out.format("%19s English %-7s", "Period #4:", "| Time: 1:55 - 3:15 \n");
 		
 		System.out.println("____________________________________________________");
 		System.out.println("");
-		System.out.format("Wednesday: %-10s Tech %7s", "Period #1:", "| Time: 9:55 - 10:55 \n");
-		System.out.format("%21s Math %-7s", "Period #2:", "| Time: 11:00 - 12:05 \n");
+		System.out.format("Wednesday: %-10s Tech    %7s", "Period #1:", "| Time: 9:55 - 10:55 \n");
+		System.out.format("%21s Math    %-7s", "Period #2:", "| Time: 11:00 - 12:05 \n");
 		System.out.format("%21s English %-7s", "Period #3:", "| Time: 1:10 - 2:10 \n");
 		System.out.format("%21s Biology %-7s", "Period #4:", "| Time: 2:15 - 3:15 \n");
 		
 		System.out.println("____________________________________________________");
 		System.out.println("");
-		System.out.format("Thursday: %-10s Math %7s", "Period #1:", "| Time: 9:00 - 10:15 \n");
-		System.out.format("%21s Tech %-7s", "Period #2:", "| Time: 10:15 - 11:40 \n");
+		System.out.format("Thursday: %-10s Math     %7s", "Period #1:", "| Time: 9:00 - 10:15 \n");
+		System.out.format("%21s Tech    %-7s", "Period #2:", "| Time: 10:15 - 11:40 \n");
 		System.out.format("%21s Biology %-7s", "Period #3:", "| Time: 12:40 - 1:55 \n");
 		System.out.format("%21s English %-7s", "Period #4:", "| Time: 1:55 - 3:15 \n");
 		
 		
 		System.out.println("");
-		System.out.format("Friday: %13s Tech %10s", "Period #1:", "| Time: 9:00 - 10:15 \n");
-		System.out.format("%21s Math %-7s", "Period #2:", "| Time: 10:15 - 11:40 \n");
+		System.out.format("Friday: %13s Tech    %10s", "Period #1:", "| Time: 9:00 - 10:15 \n");
+		System.out.format("%21s Math    %-7s", "Period #2:", "| Time: 10:15 - 11:40 \n");
 		System.out.format("%21s English %-7s", "Period #3:", "| Time: 12:40 - 1:55 \n");
 		System.out.format("%21s Biology %-7s", "Period #4:", "| Time: 1:55 - 3:15 \n");
 		
---
+++ ICS3U-HendersonE/src/unit1/BingoAssignment.java
@@ -0,0 +1,31 @@
+package unit1;
+
+public class BingoAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Start
+		System.out.println("BINGO CARD!");
+		System.out.println("");
+		System.out.println("\n1. The caller randomly pulls A numbered bingo ball.");
+		System.out.println("\n2. The Numbver is placed on the bingo board and called.");
+		System.out.println("\n3. Players look for the called number on their bingo board.");
+		System.out.println("\n4. If the number is located it is marked off.");
+		System.out.println("\n5. Steps 1 through 4 are repeated until player matches the bingo pattern.");
+		System.out.println("\n6. The winning player yells BINGO!");
+		
+		System.out.println("");
+		System.out.println("Ethan's Bingo Card:");
+		System.out.println("");
+		System.out.format("\n\t\tB\tI\tN\tG\tO");
+		System.out.println("");
+		System.out.format("\n\t\t14\t23\t34\t56\t74");
+		System.out.format("\n\t\t3\t26\t41\t50\t65");
+		System.out.format("\n\t\t8\t29\tFREE\t59\t71");
+		System.out.format("\n\t\t6\t30\t44\t60\t63");
+		System.out.format("\n\t\t15\t20\t46\t58\t75");
+	}
+
+}
+
