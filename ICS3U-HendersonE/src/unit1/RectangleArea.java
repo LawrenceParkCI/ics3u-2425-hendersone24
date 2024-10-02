73505bc67c9adf93df48962f2ffbede273056867
350258218 <350258218@C-MXL2393F1V.tdsb.on.ca>
2024-10-02 11:38:57 -0400

    Rectangle Area Sheet | Almost Complete

---
+++ ICS3U-HendersonE/src/unit1/RectangleArea.java
@@ -0,0 +1,35 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class RectangleArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//declare variables
+		int length, width, depth; 
+		
+		//get the user input
+		System.out.println("AREA PROGRAM");
+		System.out.println("Type in the length of the rectangle and <Enter>: ");
+		length = sc.nextInt();
+		
+		System.out.println("Type in the width of the rectangle and <Enter>: ");
+		width = sc.nextInt();
+		
+		System.out.println("Type in the depth of the rectangle and <Enter>: ");
+		depth = sc.nextInt();
+		
+		//calculate the volume
+		int volume = length * width * depth; 
+		
+		//print the output
+		System.out.println("The area of your rectangle is" + volume);
+		sc.close();
+
+	}
+
+}
