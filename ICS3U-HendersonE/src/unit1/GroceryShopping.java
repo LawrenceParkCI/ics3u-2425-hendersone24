5afc5fa55ad3a7f742da835ae60251d593da4769
350258218 <350258218@C-MXL2393F1V.tdsb.on.ca>
2024-10-21 10:14:11 -0400

    Still working on the assigment, Working through the receipt.

--- ICS3U-HendersonE/src/unit1/GroceryShopping.java
+++ ICS3U-HendersonE/src/unit1/GroceryShopping.java
@@ -16,6 +16,12 @@ public class GroceryShopping {
 		
 		final double tax = 0.13; 
 		
+		//First Item 
+	
+		System.out.println("|| $$$ \\\\\\ ========== \"Mr. Henderson's Groceries\" ========== /// $$$ ||"); 
+		System.out.println(); 
+		
+		System.out.println("First Item: \n");
 		System.out.print("What would you like to buy?");
 		String product = sc.nextLine();	
 		
@@ -28,8 +34,32 @@ public class GroceryShopping {
 	    var totalAmount = (price * quantity); 
 	    System.out.print("Your Total Amount Due:" + totalAmount + (totalAmount * tax));
 	    
+	    //Second Item
 	    
+	    System.out.println(); 
 	    
+	    System.out.println("Item 2: \n"); 
+	    System.out.println(); 
+	    
+	    System.out.println("What would you like to buy?"); 
+	    String product2 = sc.next();
+	    
+	    System.out.print("How much does it cost?");
+		double price2 = sc.nextDouble();
+	    
+		System.out.print("How many will you be buying?");
+	    int quantity2 = sc.nextInt(); 
+	    
+	    var totalAmount2 = (price2 * quantity2); 
+	    System.out.print("Your Total Amount Due:" + totalAmount2 + (totalAmount2 * tax));
+	    
+	    //Receipt 
+	    
+	   System.out.printf("$65s", "This is your recipt /n"); 
+	   System.out.printf("$75s", "Item      |  Price  | Quant. | Total Price "); 
+	   System.out.printf("$63s", "------------------------------------------");
+	   System.out.printf("%32s %11s %10s %14s ");
+
 	
 	}
 
